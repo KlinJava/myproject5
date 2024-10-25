883602d1c025e351d93c7a8680c0c062fdd96d6a
KlinJava <alarmpinglin@gmail.com>
2024-10-25 18:23:32 +0800

    Data type

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.println("Hello!");
         Person p = new Person();
         p.hello();
+        int age = 19; //32bits
+        char c = '我'; //16bits
+        byte b = 120; //8bits
+        float weight = 66.5f; //32bits
+        double height = 1.7; //64bits
+        boolean adult = true;
+        boolean enroll = false;
+
+
+
+
     }
 }
