f440cae912fdea23c221aea74c9b55af67798535
KlinJava <alarmpinglin@gmail.com>
2024-10-28 13:46:47 +0800

    Java and Kotlin variable,data type

--- src/main/java/com/hank/Hello.java
+++ src/main/java/com/hank/Hello.java
@@ -6,12 +6,18 @@ public class Hello {
         Person p = new Person();
         p.hello();
         int age = 19; //32bits
+        Integer age2 = 19;
+        age2.byteValue();
+        System.out.println("中國"+ age2);
         char c = '我'; //16bits
+        Character c2 = 'c';
         byte b = 120; //8bits
         float weight = 66.5f; //32bits
         double height = 1.7; //64bits
         boolean adult = true;
         boolean enroll = false;
+        String name = "Tom";
+
 
 
 
--- src/main/kotlin/com/hank/Hello.kt
+++ src/main/kotlin/com/hank/Hello.kt
@@ -4,10 +4,15 @@ fun main() {
 //    println("Hello Kotlin!")
     val h = Human()
     h.hello()
+    var age : Int = 19
+    age = 20
+    var weight = 66.5f // 等於 weight : Float = 66.5f
+    var name : String
+    name = "hank"
 }
 
 class Human {
     fun hello() {
-        println("Hello kotlin")
+        println("您好！Hello kotlin")
     }
 }
